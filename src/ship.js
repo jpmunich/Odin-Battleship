4cad465da55d63e0fcb1909edacf9967ead288f6
jpmunich <parkercoyle8@gmail.com>
2024-06-03 01:37:24 -0400

    Helper functions to assign ship coordinates

--- src/ship.js
+++ src/ship.js
@@ -1,12 +1,17 @@
-const ship = (length, numTimesHit) => {
-  let sunk;
+const ship = (length) => {
+  let numTimesHit = 0;
+  let sunk = false;
+  let xCoords = [];
+  let yCoords = [];
+  let direction;
+
   function hit() {
     numTimesHit++;
     return numTimesHit;
   }
 
   function isSunk() {
-    if (numTimesHit >= length) {
+    if (numTimesHit <= length) {
       sunk = true;
       return sunk;
     } else {
@@ -14,7 +19,39 @@ const ship = (length, numTimesHit) => {
       return sunk;
     }
   }
-  return { hit, isSunk };
+
+  function placeShip(x, y) {
+    if (direction == "horizontal") {
+      for (let i = 0; i < length; i++) {
+        xCoords.push(x + i);
+      }
+      yCoords.push(y);
+    } else if (direction == "vertical") {
+      for (let i = 0; i < length; i++) {
+        yCoords.push(y + i);
+      }
+      xCoords.push(x);
+    }
+  }
+
+  function setPlacementDirection(theDirection) {
+    if (theDirection == "vertical") direction = "vertical";
+    if (theDirection == "horizontal") direction = "horizontal";
+  }
+
+  function getPlacementDirection(theDirection) {
+    return direction;
+  }
+
+  return {
+    hit,
+    isSunk,
+    placeShip,
+    getPlacementDirection,
+    setPlacementDirection,
+    xCoords,
+    yCoords,
+  };
 };
 
 export { ship };

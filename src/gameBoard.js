876389dbbabdf714f77de24fed4a52472358ab69
jpmunich <parkercoyle8@gmail.com>
2024-06-06 15:07:01 -0400

    Added grid bounds + prevented ship overlap

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -15,17 +15,35 @@ const gameBoard = () => {
   ];
 
   let ships = [];
+
   function placeShip(shipLength, axis, x, y) {
+    // Set bounds of grid to be 1-10
+    if (x < 1 || x > 10 || y < 1 || y > 10) return "Coordinates Out of Bounds";
+
     const theShip = ship(shipLength);
     theShip.setPlacementDirection(axis);
     theShip.placeSelf(x, y);
     ships.push(theShip);
 
     for (let i = 0; i < theShip.getShipLength(); i++) {
+      // Check to make sure two ships do not overlap
+      if (
+        grid[theShip.getYCoords()[i] - 1][theShip.getXCoords()[i] - 1] == "S"
+      ) {
+        ships.pop();
+        for (let j = 0; j < i; j++) {
+          grid[theShip.getYCoords()[j] - 1][theShip.getXCoords()[j] - 1] = "";
+        }
+        return grid;
+      }
+
       grid[theShip.getYCoords()[i] - 1][theShip.getXCoords()[i] - 1] = "S";
     }
+
     return grid;
   }
+
+  function receiveAttack(x, y) {}
 
   return { placeShip, grid };
 };
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -17,3 +17,35 @@ test("place ship assigns all coordinates based on ship length", () => {
     ["", "", "", "", "", "", "", "", "", ""],
   ]);
 });
+
+test("place ship returns error message if coordinates are too high", () => {
+  const theGameBoard = gameBoard();
+  expect(theGameBoard.placeShip(4, "horizontal", 11, 4)).toBe(
+    "Coordinates Out of Bounds"
+  );
+});
+
+test("place ship returns error message if coordinates are too low", () => {
+  const theGameBoard = gameBoard();
+  expect(theGameBoard.placeShip(4, "horizontal", 0, 4)).toBe(
+    "Coordinates Out of Bounds"
+  );
+});
+
+test("Cannot place ship on top of other ship", () => {
+  const theGameBoard = gameBoard();
+  theGameBoard.placeShip(4, "vertical", 5, 5);
+
+  expect(theGameBoard.placeShip(5, "horizontal", 3, 7)).toEqual([
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "S", "", "", "", "", ""],
+    ["", "", "", "", "S", "", "", "", "", ""],
+    ["", "", "", "", "S", "", "", "", "", ""],
+    ["", "", "", "", "S", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+  ]);
+});
